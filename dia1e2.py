b87720ae893a505533731fd14e77971740b7df16
Gabriel Siqueira <gabriel.siqueira@globant.com>
2024-12-17 20:11:00 -0300

    adicionando conteúdo de lógica básica no dia 1 e 2

---
+++ dia1e2.py
@@ -0,0 +1,31 @@
+#======= variaveis =======
+idade = 23; #tipo int recebe valores do tipo inteiro, não aceita numeros reais.
+nome = "Gabriel";
+altura=1.82; #tipo float é um tipo numérico que aceita valores reais (numeros com virgulas).
+
+# ======= tipos de dados ======
+#int, float, strings, bool
+cidade = "São paulo" #tipo string recebe sempre um array de caracteres.
+
+esta_logado = True; #tipos booleanos servem para verificar se uma condição é aceitada.
+
+print(nome);
+nome = 2 #como pythobn não é uma lingugagem tipada, não é preciso declarar variaveis, podendo assim serem substituidas por outros tipos de dados e valores.
+
+print(nome);
+# metodo type() verifica que tipo de dado aquela variável possui.
+print(type(idade)); #int
+print(type(nome)); #string | int
+print(type(altura)); #float
+print(type(cidade)); #string
+print(type(esta_logado)); #boolean
+
+#======= Operadores  matemáticos =======
+print( 5 + 12);
+print( 51 - 12);
+print( 10 * 5);
+print( 85 / 5);
+
+#======= Concatenação =======
+# usar o + para concatenar 2 strings.
+
